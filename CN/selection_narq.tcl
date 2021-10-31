fdc9b2d53532d9bf424f98dc346f7829eac2204e
kituu02 <68700179+kituu02@users.noreply.github.com>
2021-10-31 14:40:20 +0530

    Create selection_narq.tcl

---
+++ CN/selection_narq.tcl
@@ -0,0 +1,52 @@
+set ns [new Simulator]
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+
+$ns at 0.0 "$n0 label Sender"
+$ns at 0.0 "$n1 label Receiver"
+$n0 color red
+$n1 color blue
+
+set nf [open Go-Back-N.nam w]
+$ns namtrace-all $nf
+set f [open Go-Back-N.tr w]
+$ns trace-all $f
+
+$ns duplex-link $n0 $n2 0.2Mb 200ms DropTail
+$ns duplex-link $n2 $n3 0.2Mb 200ms DropTail
+$ns duplex-link $n3 $n1 0.2Mb 200ms DropTail
+
+
+Agent/TCP set nam_tracevar_ true
+
+set tcp [new Agent/TCP]
+$tcp set windowInit_ 10
+$tcp set maxcwnd_ 10
+$ns attach-agent $n0 $tcp
+
+set sink [new Agent/TCPSink]
+
+$ns attach-agent $n1 $sink
+
+$ns connect $tcp $sink
+$ns rtmodel-at 1.80 down $n2 $n3
+$ns rtmodel-at 1.91 up $n2 $n3
+set ftp [new Application/FTP]
+$ftp attach-agent $tcp
+$ns at 0.1 "$ftp start"
+$ns at 10.68 "$ftp stop"
+$ns at 10.8 "$ns detach-agent $n0 $tcp ; $ns detach-agent $n1 $sink"
+$ns at 11.0 "finish"
+
+proc finish {} {
+        global ns
+        $ns flush-trace
+       puts "running nam..."
+       exec nam Go-Back-N.nam &
+        exit 0
+}
+
+$ns run
