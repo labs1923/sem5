d77b3212bc9e2075e73e2e87cadae1704f920bc2
kituu02 <68700179+kituu02@users.noreply.github.com>
2021-11-01 15:10:38 +0530

    Update udp2udp.tcl

--- CN/udp2udp.tcl
+++ CN/udp2udp.tcl
@@ -9,7 +9,7 @@ set file2 [open udpout1.nam w]
 $ns namtrace-all $file2 
 #Define a 'finish' procedure 
 proc finish {} { 
-global ns  file2 
+global ns file1 file2 
 $ns flush-trace 
 close $file1 
 close $file2 
@@ -38,9 +38,15 @@ $ns duplex-link $n1 $n4 2Mb 10ms DropTail
 #Setup a TCP connection 
 set udp [new Agent/UDP] 
 $ns attach-agent $n0 $udp
+set null [new Agent/Null]
+$ns attach-agent $n1 $null
+$ns connect $udp $null
 set udp1 [new Agent/UDP] 
 $ns attach-agent $n1 $udp1 
-$ns connect $udp $udp1
+set null1 [new Agent/Null]
+$ns attach-agent $n0 $null1
+$ns connect $udp1 $null1
+
 $udp set fid_ 1 
 
 $udp1 set fid_ 2 
@@ -57,14 +63,10 @@ $cbr1 set type_ CBR
 #$cbr1 set window_ 2 
 
 
-$ns at 0.4 "$cbr start" 
-$ns at 0.4 "$cbr1 start" 
-$ns at 0.47 "$cbr stop" 
-$ns at 0.47 "$cbr1 stop" 
-$ns at 0.48 "$cbr start" 
-$ns at 0.48 "$cbr1 start" 
-$ns at 0.55 "$cbr stop" 
-$ns at 0.55 "$cbr1 stop"
+$ns at 0.1 "$cbr start" 
+$ns at 0.1 "$cbr1 start" 
+$ns at 0.5 "$cbr stop" 
+$ns at 0.5 "$cbr1 stop" 
+
 $ns at 13.05 "finish" 
 $ns run
-
