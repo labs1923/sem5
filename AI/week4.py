f924df0dd5631ac8c8e0fb82ab2972fe905eecc3
SoulCynic <91052431+SoulCynic@users.noreply.github.com>
2021-12-30 22:34:42 +0530

    Update week4.py

--- AI/week4.py
+++ AI/week4.py
@@ -1,23 +1,23 @@
-n=int(input("Enter no. of nodes: "))
-heuristic=[0]+list(map(int,input("Enter Heuristic values: ").split()))
-cost=[0,]
-path=[]
+n = int(input('Enter number of nodes : '))
+heuristic = [0]+list(map(int,input("Enter Heuristic values : ").split()))
+cost = [0]
+path = []
 for x in range(n):
- cost.append([0]+list(map(int,input().split())))
-sour=int(input("Enter the source: "))
-dest=int(input("Enter the destination: "))
-current=sour
-sum=0
-while current!=dest:
- min=9999
- minval=0
- for i in range(1,n+1):
-   x=heuristic[i]+cost[current][i]
-   if x<min and i not in path: 
-     min=x
-     minval=i
- sum+=min
- path.append(current)
- current=minval
-path.append(dest)
+  cost.append([0]+list(map(int,input().split())))
+src = int(input('Enter the Source : '))
+des = int(input('Enter the destination : '))
+current = src
+sum = 0
+while(current != des):
+  min = 9999
+  minval = 0
+  for i in range(1,n+1):
+    x = heuristic[i] + cost[current][i]
+    if x<min and i not in path:
+      min = x
+      minval = i
+  sum += min
+  path.append(current)
+  current = minval
+path.append(des)
 print(path)
