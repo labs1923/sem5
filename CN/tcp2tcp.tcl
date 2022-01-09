e77821e6f0748adbcdd1c8878f8b8ac90ec61acb
Rakesh <60481830+deephunt3r@users.noreply.github.com>
2022-01-09 16:40:27 +0530

    Update tcp2tcp.tcl

--- CN/tcp2tcp.tcl
+++ CN/tcp2tcp.tcl
@@ -2,16 +2,14 @@
 set ns [new Simulator] 
 $ns color 1 Blue 
 $ns color 2 Red 
-#Open the Trace files 
-#set file1 [open out.tr w] 
-#$ns trace-all $file1 
+
 set file2 [open out.nam w] 
 $ns namtrace-all $file2 
-#Define a 'finish' procedure 
+
 proc finish {} { 
 global ns  file2 
 $ns flush-trace 
-#close $file1 
+
 close $file2 
 exec nam out.nam & 
 exit 0 
@@ -28,17 +26,21 @@ $ns duplex-link $n1 $n2 10Mb 10ms DropTail
 #Setup a TCP connection 
 set tcp [new Agent/TCP/Newreno] 
 $ns attach-agent $n0 $tcp 
+
 set tcp1 [new Agent/TCP/Newreno] 
 $ns attach-agent $n1 $tcp1 
 $ns connect $tcp $tcp1
+
 $tcp set fid_ 1 
 $tcp set window_ 2 
 $tcp1 set fid_ 2 
 $tcp1 set window_ 2 
+
 #Setup a FTP over TCP connection 
 set ftp [new Application/FTP] 
 $ftp attach-agent $tcp 
 $ftp set type_ FTP 
+
 set ftp1 [new Application/FTP] 
 $ftp1 attach-agent $tcp1
 $ftp1 set type_ FTP 
